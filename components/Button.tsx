d1b002fe7863f6442d802d976d157df79b093925
Adrien Pauchet <grandchefdotkong@gmail.com>
2023-11-28 17:16:27 +0900

    herro section added

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
   type: 'button' | 'submit',
   title: string,
   icon?: string,
-  variant: 'btn_dark_green'
+  variant: 'btn_dark_green' | 'btn_green' | 'btn_white'
 }
 
 const Button = ({ type, title, icon, variant }: ButtonProps) => {
@@ -14,7 +14,7 @@ const Button = ({ type, title, icon, variant }: ButtonProps) => {
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="blod-16 whitespace-wrap">{ title }</label>
+      <label className="blod-16 whitespace-wrap cursor-pointer">{ title }</label>
     </button>
   )
 }
