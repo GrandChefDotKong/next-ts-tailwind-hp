4934a22056dced0bb3db1c0fdfb0dad170f82c15
Adrien Pauchet <grandchefdotkong@gmail.com>
2023-11-29 15:13:12 +0900

    Camp section added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className="border-2 border-red-200 max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image 
@@ -50,7 +50,6 @@ const Hero = () => {
       </div>
       <div className="relative flex flex-1 items-start">
         <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
-
            <div className="flex flex-col">
             <div className="flexBetween">
               <p className="regular-16 text-gray-20">Location</p>
